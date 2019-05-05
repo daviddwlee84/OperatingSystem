f2b30f902ce477ac18de19fd6cd38651eb65bc39
David Lee <daviddwlee84@gmail.com>
2019-05-05 17:02:59 +0800

    Lab4 fibonacci user program

---
+++ Nachos/nachos-3.4/code/test/fibonacci.c
@@ -0,0 +1,22 @@
+/* fibonacci.c
+ *	Simple program to test the TLB miss rate (Lab 4) that calculate the fibonacci series
+ */
+
+#include "syscall.h"
+
+#define N 20
+
+int
+main()
+{
+    int i;
+    int result[N];
+    result[0] = 0;
+    result[1] = 1;
+    for (i = 2; i < N; i++)
+    {
+        result[i] = result[i-1] + result[i-2];
+    }
+    // Exit(result[i]); // because we haven't implement Exit syscall yet
+    Halt();
+}
