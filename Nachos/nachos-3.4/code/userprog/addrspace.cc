8f66a19dcd30012f485feb1ee78d12273525304f
David Lee <daviddwlee84@gmail.com>
2019-05-10 21:18:54 +0800

    Update init of AddrSpace for demand paging

--- Nachos/nachos-3.4/code/userprog/addrspace.cc
+++ Nachos/nachos-3.4/code/userprog/addrspace.cc
@@ -78,43 +78,75 @@ AddrSpace::AddrSpace(OpenFile *executable)
     numPages = divRoundUp(size, PageSize);
     size = numPages * PageSize;
 
+#ifndef DEMAND_PAGING
     ASSERT(numPages <= NumPhysPages);		// check we're not trying
 						// to run anything too big --
 						// at least until we have
 						// virtual memory
+#else
+    // Create a virtual memory with the size that the executable file need.
+    DEBUG('a', "Demand paging: creating virtual memory!\n");
+    bool success_create_vm = fileSystem->Create("VirtualMemory", size);
+    ASSERT_MSG(success_create_vm, "fail to create virtual memory");
+#endif
 
-    DEBUG('a', "Initializing address space, num pages %d, size %d\n", 
+    DEBUG('a', "Initializing address space (page table), num pages %d, size %d\n", 
 					numPages, size);
 // first, set up the translation 
     pageTable = new TranslationEntry[numPages];
     for (i = 0; i < numPages; i++) {
-	pageTable[i].virtualPage = i;	// for now, virtual page # = phys page #
+	pageTable[i].virtualPage = i;	// for now (default), virtual page # = phys page #
 
-// Lab4: Global data structure for memory management
-#if USE_BITMAP
+#ifndef DEMAND_PAGING // Lab4: Lazy-loading (Demand paging)
+#if USE_BITMAP // Lab4: Global data structure for memory management
     pageTable[i].physicalPage = machine->allocateFrame();
     ASSERT_MSG(pageTable[i].physicalPage != -1, "No free page frame."); // Currently, all page need to be loaded into the memory
 #elif USE_LINKED_LIST
     // TODO
-#else
-	pageTable[i].physicalPage = i; // Original linear allocation
-#endif
+#else // not using data structure (i.e. Original linear allocation)
+	pageTable[i].physicalPage = i;
+#endif // Free memory space data structure
 	pageTable[i].valid = TRUE;
+#else // Demand Paging
+    pageTable[i].valid = FALSE;
+#endif
 	pageTable[i].use = FALSE;
 	pageTable[i].dirty = FALSE;
 	pageTable[i].readOnly = FALSE;  // if the code segment was entirely on 
 					// a separate page, we could set its 
 					// pages to be read-only
     }
-#if USE_BITMAP
+#if USE_BITMAP && !DEMAND_PAGING
     DEBUG('M', "Bitmap after allocate: %08X\n", machine->bitmap);
 #endif
     
-// zero out the entire address space, to zero the unitialized data segment 
+#ifndef DEMAND_PAGING // If use lazy loading then don't load executable into memory at first.
+// zero out the entire (needed) address space (size), to zero the unitialized data segment 
 // and the stack segment
     bzero(machine->mainMemory, size);
 
-// then, copy in the code and data segments into memory
+#if USE_BITMAP
+    // TODO: Current memroy load sequentially, but what we need is load the memory into
+    //       the page that allocate by bitmap.
+
+    // char *tempMemory;
+    // tempMemory = new char[size];
+    // for (i = 0; i < size; i++)
+    //     tempMemory[i] = 0;
+
+    // if (noffH.code.size > 0) {
+    //     DEBUG('a', "Initializing code segment, at 0x%x, size %d\n", 
+	// 		noffH.code.virtualAddr, noffH.code.size);
+    //     executable->ReadAt(&(tempMemory[noffH.code.virtualAddr]),
+	// 		noffH.code.size, noffH.code.inFileAddr);
+    // }
+    // if (noffH.initData.size > 0) {
+    //     DEBUG('a', "Initializing data segment, at 0x%x, size %d\n", 
+	// 		noffH.initData.virtualAddr, noffH.initData.size);
+    //     executable->ReadAt(&(tempMemory[noffH.initData.virtualAddr]),
+	// 		noffH.initData.size, noffH.initData.inFileAddr);
+    // }
+
     if (noffH.code.size > 0) {
         DEBUG('a', "Initializing code segment, at 0x%x, size %d\n", 
 			noffH.code.virtualAddr, noffH.code.size);
@@ -127,6 +159,53 @@ AddrSpace::AddrSpace(OpenFile *executable)
         executable->ReadAt(&(machine->mainMemory[noffH.initData.virtualAddr]),
 			noffH.initData.size, noffH.initData.inFileAddr);
     }
+#else // original version
+    // then, copy in the code and data segments into memory
+    if (noffH.code.size > 0) {
+        DEBUG('a', "Initializing code segment, at 0x%x, size %d\n", 
+			noffH.code.virtualAddr, noffH.code.size);
+        executable->ReadAt(&(machine->mainMemory[noffH.code.virtualAddr]),
+			noffH.code.size, noffH.code.inFileAddr);
+    }
+    if (noffH.initData.size > 0) {
+        DEBUG('a', "Initializing data segment, at 0x%x, size %d\n", 
+			noffH.initData.virtualAddr, noffH.initData.size);
+        executable->ReadAt(&(machine->mainMemory[noffH.initData.virtualAddr]),
+			noffH.initData.size, noffH.initData.inFileAddr);
+    }
+#endif // USE_BITMAP
+#else // Use demand paging
+// zero out the entire address space (MemorySize), to zero the unitialized data segment 
+// and the stack segment
+    bzero(machine->mainMemory, MemorySize);
+
+    DEBUG('a', "Demand paging: copy executable to virtual memory!\n");
+
+    OpenFile *vm = fileSystem->Open("VirtualMemory");
+
+    char *virtualMemory_temp;
+    virtualMemory_temp = new char[size];
+    for (i = 0; i < size; i++)
+        virtualMemory_temp[i] = 0;
+
+    if (noffH.code.size > 0) {
+        DEBUG('a', "\tCopying code segment, at 0x%x, size %d\n",
+              noffH.code.virtualAddr, noffH.code.size);
+        executable->ReadAt(&(virtualMemory_temp[noffH.code.virtualAddr]),
+                           noffH.code.size, noffH.code.inFileAddr);
+        vm->WriteAt(&(virtualMemory_temp[noffH.code.virtualAddr]),
+                    noffH.code.size, noffH.code.virtualAddr*PageSize);
+    }
+    if (noffH.initData.size > 0) {
+        DEBUG('a', "\tCopying data segment, at 0x%x, size %d\n",
+              noffH.initData.virtualAddr, noffH.initData.size);
+        executable->ReadAt(&(virtualMemory_temp[noffH.initData.virtualAddr]),
+                           noffH.initData.size, noffH.initData.inFileAddr);
+        vm->WriteAt(&(virtualMemory_temp[noffH.initData.virtualAddr]),
+                    noffH.initData.size, noffH.initData.virtualAddr*PageSize);
+    }
+    delete vm; // Close the file
+#endif
 
 }
 
